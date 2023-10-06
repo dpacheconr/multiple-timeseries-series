a5b4c6f78c6618fdec40287f7258d4b134341703
dpacheconr <dpacheco@newrelic.com>
2023-10-06 08:07:00 +0100

    com

--- visualizations/multi-timeseries-series/index.js
+++ visualizations/multi-timeseries-series/index.js
@@ -75,6 +75,7 @@ function unixtodatetime(data) {
 }
  
 function exportToCsv (data){
+    console.log(data)
     let newdata = []
 
     data.forEach(el => {
@@ -137,12 +138,13 @@ function parse_data(array) {
     return sets
 }
 
-function parse_time_string(data){
-    let dateObj = new Date(data);
-    let utcString = dateObj.toUTCString();
-    let time = utcString.slice(5,-4);
-    return time
-}
+// function parse_time_string(data){
+//     let dateObj = new Date(data);
+//     let utcString = dateObj.toUTCString();
+//     let time = utcString.slice(5,-4);
+//     return time
+// }
+
 function calculatedata(data) {
    
     let resultarray = []
@@ -152,6 +154,8 @@ function calculatedata(data) {
     let trimmedminctrl = []
     let trimmedmaxctrl = []
     let trimmedareactrl = []
+    let clippedminctrl = []
+    let clippedmaxctrl = []
     let clippedareactrl = []
     let avgarrctrl = []
     let minmaxareactrl=[]
@@ -178,7 +182,8 @@ function calculatedata(data) {
     let minmaxmaxs = []
 
     let clippedarea = []
-
+    let clippedmin = []
+    let clippedmax = []
 
 
         resultarray.forEach((yvalues) => {
@@ -204,7 +209,11 @@ function calculatedata(data) {
 
                 //determine clipped area
                 let clippedData=clipFunction(yvalues,parseInt(clipSize))
-                clippedarea.push(getMinMax(clippedData))
+                let c_area = getMinMax(clippedData)
+                clippedarea.push(c_area)
+                clippedmin.push(c_area[0])
+                clippedmax.push(c_area[1])
+
 
         })
 
@@ -221,6 +230,8 @@ function calculatedata(data) {
             minmaxminsctrl.push(build_json(z,i,minmaxmins,"min"))
             minmaxmaxsctrl.push(build_json(z,i,minmaxmaxs,"max"))
 
+            clippedminctrl.push(build_json(z,i,clippedmin,"clippedmin"))
+            clippedmaxctrl.push(build_json(z,i,clippedmax,"clippedmax"))
             clippedareactrl.push(build_json(z,i,clippedarea,"clippedarea"))
 
         }
@@ -233,24 +244,28 @@ function calculatedata(data) {
     let minset = parse_data (minmaxminsctrl)
     let maxset = parse_data (minmaxmaxsctrl)
     let clippedareaset = parse_data(clippedareactrl)
+    let clippedminset = parse_data(clippedminctrl)
+    let clippedmaxset = parse_data(clippedmaxctrl)
 
 
 
     // update queries with calculated data
     data.push({"data":[{"data":avgset, "metadata":{"viz":"main","name": "avg","id":"74B5B05EEA583471E03DCBF0123D81CC79CAE0FE9", "color": defaultColors[1]}}],loading: false, error: null})
-    data.push({"data":[{"data":trimmedareaset, "metadata":{"viz":"main","name": "trimmedarea","id":"74B5B05EEA583471E03DCBF0123D81CC79CEE0FE9", "color":defaultColors[1]}}],loading: false, error: null})
+    data.push({"data":[{"data":trimmedareaset, "metadata":{"viz":"main","name": "trimmedarea","id":"74B5B05EEA583471E03DCBF0123D81CC79CEE0FE9", "color":defaultColors[2]}}],loading: false, error: null})
     data.push({"data":[{"data":trimmedminset, "metadata":{"viz":"main","name": "trimmedmin","id":"02D6A84F7B97E4709A11276615FDAAB3EE2BEE415", "color": defaultColors[2]}}],loading: false, error: null})
     data.push({"data":[{"data":trimmedmaxset, "metadata":{"viz":"main","name": "trimmedmax","id":"2C1F4F2BAA2800FD80F50C3811F38D03B52DEEEB1", "color":defaultColors[2]}}],loading: false, error: null})
-    data.push({"data":[{"data":minmaxareaset, "metadata":{"viz":"main","name": "minmaxarea","id":"74B5B05EEA583471E03DCBF0123D81CC79CDE0FE9", "color": defaultColors[9]}}],loading: false, error: null})
-    data.push({"data":[{"data":clippedareaset, "metadata":{"viz":"main","name": "clippedarea","id":"74B5B05EEA583471E03DCBF0123D81CC79CDE0FE8", "color": defaultColors[10]}}],loading: false, error: null})
+    data.push({"data":[{"data":clippedareaset, "metadata":{"viz":"main","name": "clippedarea","id":"74B5B05EEA583471E03DCBF0123D81CC79CDE0JE8", "color": defaultColors[10]}}],loading: false, error: null})
+    data.push({"data":[{"data":clippedminset, "metadata":{"viz":"main","name": "clippedmin","id":"74B5B05EEA583471E03DCBF0123D81CC79CDE0LE8", "color": defaultColors[10]}}],loading: false, error: null})
+    data.push({"data":[{"data":clippedmaxset, "metadata":{"viz":"main","name": "clippedmax","id":"74B5B05EEA583471E03DCBF0123D81CC79CDE0FE8", "color": defaultColors[10]}}],loading: false, error: null})
+    data.push({"data":[{"data":minmaxareaset, "metadata":{"viz":"main","name": "minmaxarea","id":"74B5B05EEA583471E03DCBF0123D81CC79CDE0FE9", "color": defaultColors[11]}}],loading: false, error: null})
     data.push({"data":[{"data":minset, "metadata":{"viz":"main","name": "min","id":"625D011FAC794651F25160AD89612DFAAE954C0CB", "color":defaultColors[11]}}],loading: false, error: null})
-    data.push({"data":[{"data":maxset, "metadata":{"viz":"main","name": "max","id":"DDB4E3844C923B3F794EC52642E22CBE9FC8D8D31", "color": defaultColors[12]}}],loading: false, error: null})
+    data.push({"data":[{"data":maxset, "metadata":{"viz":"main","name": "max","id":"DDB4E3844C923B3F794EC52642E22CBE9FC8D8D31", "color": defaultColors[11]}}],loading: false, error: null})
    
 }
 
 const duration = 60*60*1000
 const enddate =  Date.now() - duration
-const startdate = enddate - duration*2
+const startdate = enddate - duration
 
 // // Often provided by the PlatformState provider
 const timeRange = {
@@ -265,19 +280,11 @@ const ctx = {tvMode: false, accountId: c_accountid, filters: undefined, timeRang
 function AlignedTimeseries(props) {
     const {nrqlQueries, alignment} = props;
     const [queryResults, setQueryResults] = useState(null);
-    let cplatformstatecontext
-    
-    try {
-        cplatformstatecontext = useContext(PlatformStateContext);
-        console.log("unable to obtain ctx",cplatformstatecontext)
-        
-    } catch (e) {
-        console.log(e)
-        cplatformstatecontext = ctx
-    }
+    const cplatformstatecontext = useContext(PlatformStateContext);
     console.log("running ctx is ", cplatformstatecontext)
+
     useEffect(async () => {      
-            let windowsize = 180000
+            let windowsize
             c_accountid = props.nrqlQueries[0].accountId
             let mainquery = props.nrqlQueries[0].query
             let compare= props.nrqlQueries[0].compare
@@ -287,14 +294,6 @@ function AlignedTimeseries(props) {
             trimmedareabol = props.nrqlQueries[0].trimmedareabol
             clippedareabol = props.nrqlQueries[0].clippedareabol
             hideoriginaldata = props.nrqlQueries[0].hideoriginaldata
-            if (cplatformstatecontext.timeRange != undefined) {
-                if (cplatformstatecontext.timeRange.duration == null){
-                    windowsize = (parseInt(cplatformstatecontext.timeRange.end_time) - parseInt(cplatformstatecontext.timeRange.begin_time)) / 1000
-                } else {
-                  windowsize = parseInt(cplatformstatecontext.timeRange.duration) / 1000
-                }
-                
-            }
 
             if (props.nrqlQueries[0].trimpercent != "") {
                 trimpercent = props.nrqlQueries[0].trimpercent
@@ -302,32 +301,46 @@ function AlignedTimeseries(props) {
             if (props.nrqlQueries[0].clipsize != "") {
                 clipSize = props.nrqlQueries[0].clipsize
             }
-           
-            // generate the compare queries
-            if (cplatformstatecontext.timeRange.duration == null) {
-                windowsize = windowsize*1000
-                for (let i = 0; i <= compare; i++) {
-                    if (i == 0 ) { 
-                        let from = parseInt((cplatformstatecontext.timeRange.end_time) - windowsize)
-                        let until = parseInt(cplatformstatecontext.timeRange.end_time)
-                        // console.log("from",parse_time_string(from))
-                        // console.log("until",parse_time_string(until))
-                        let query = mainquery + " SINCE " + from + " until "+ until  + " TIMESERIES " + timeseries
+            
+            if (cplatformstatecontext.timeRange != undefined) {
+                if (cplatformstatecontext.timeRange.duration == null){
+                    windowsize = (parseInt(cplatformstatecontext.timeRange.end_time) - parseInt(cplatformstatecontext.timeRange.begin_time)) / 1000
+                    windowsize = windowsize*1000
+                    for (let i = 0; i <= compare; i++) {
+                        if (i == 0 ) { 
+                            let from = parseInt((cplatformstatecontext.timeRange.end_time) - windowsize)
+                            let until = parseInt(cplatformstatecontext.timeRange.end_time)
+                            // console.log("from",parse_time_string(from))
+                            // console.log("until",parse_time_string(until))
+                            let query = mainquery + " SINCE " + from + " until "+ until  + " TIMESERIES " + timeseries
+                            props.nrqlQueries[0].query = query
+                        } else {
+                            let from = parseInt(cplatformstatecontext.timeRange.end_time-(windowsize*(i+1)))
+                            let until = parseInt(cplatformstatecontext.timeRange.end_time-windowsize*(i))
+                            // console.log("from",i,parse_time_string(from))
+                            // console.log("until",i,parse_time_string(until))
+                            let query = mainquery + " SINCE " + from + " until "+ until + " TIMESERIES " + timeseries
+                            nrqlQueries.push({accountId: c_accountid, query: query, color: defaultColors[Math.floor(Math.random() * defaultColors.length)]})
+                            
+                            // console.log("until1",parse_time_string(until))
+                            // console.log("query1 ", query)
+                        }                
+                    }
+                } else {
+                  windowsize = parseInt(cplatformstatecontext.timeRange.duration) / 1000
+                  for (let i = 0; i <= compare; i++) {
+                    let query = mainquery + " SINCE " + (parseInt(windowsize)*(i+1))  + " seconds ago " + " until "+ (parseInt(windowsize)*(i))  + " seconds ago TIMESERIES " + timeseries
+                    if (i == 0 ) {
                         props.nrqlQueries[0].query = query
                     } else {
-                        let from = parseInt(cplatformstatecontext.timeRange.end_time-(windowsize*(i+1)))
-                        let until = parseInt(cplatformstatecontext.timeRange.end_time-windowsize*(i))
-                        // console.log("from",i,parse_time_string(from))
-                        // console.log("until",i,parse_time_string(until))
-                        let query = mainquery + " SINCE " + from + " until "+ until + " TIMESERIES " + timeseries
                         nrqlQueries.push({accountId: c_accountid, query: query, color: defaultColors[Math.floor(Math.random() * defaultColors.length)]})
-                        
-                        // console.log("until1",parse_time_string(until))
-                        // console.log("query1 ", query)
+                    
                     }                
                 }
+                }
+                
             } else {
-
+                windowsize = 1800 // defaults to 30 minutes
                 for (let i = 0; i <= compare; i++) {
                     let query = mainquery + " SINCE " + (parseInt(windowsize)*(i+1))  + " seconds ago " + " until "+ (parseInt(windowsize)*(i))  + " seconds ago TIMESERIES " + timeseries
                     if (i == 0 ) {
@@ -337,7 +350,7 @@ function AlignedTimeseries(props) {
                     
                     }                
                 }
-        }
+            }
 
 
             let promises=nrqlQueries.map((q)=>{return NrqlQuery.query({accountIds: [q.accountId], query: q.query,formatTypeenum: NrqlQuery.FORMAT_TYPE.CHART})})
@@ -364,10 +377,7 @@ function AlignedTimeseries(props) {
             })
 
             calculatedata(data)
-
             setQueryResults(data)
-
-
 
             // let refreshratems
             // if (timeseries.includes("second")) { // if the window size is 1 hour or more
@@ -443,7 +453,7 @@ function AlignedTimeseries(props) {
         let arechartdata = []
 
         queryResults.forEach(r=>{ if(r.data && r.data[0] && r.data[0].metadata.name != "trimmedarea" && r.data[0].metadata.name != "minmaxarea" && r.data[0].metadata.name != "clippedarea") {exportchartData.push(r.data[0])}}) 
-        queryResults.forEach(r=>{ if(r.data && r.data[0] && (r.data[0].metadata.name != "min" && r.data[0].metadata.name != "max" && r.data[0].metadata.name != "trimmedmin" && r.data[0].metadata.name != "trimmedmax" && r.data[0].metadata.name != "avg" && r.data[0].metadata.name != "trimmedarea" && r.data[0].metadata.name != "minmaxarea" && r.data[0].metadata.name != "clippedarea") ){vizchartData.push(r.data[0])}}) 
+        queryResults.forEach(r=>{ if(r.data && r.data[0] && (r.data[0].metadata.name != "min" && r.data[0].metadata.name != "max" && r.data[0].metadata.name != "trimmedmin" && r.data[0].metadata.name != "trimmedmax" && r.data[0].metadata.name != "avg" && r.data[0].metadata.name != "trimmedarea" && r.data[0].metadata.name != "minmaxarea" && r.data[0].metadata.name != "clippedarea" && r.data[0].metadata.name != "clippedmin" && r.data[0].metadata.name != "clippedmax") ){vizchartData.push(r.data[0])}}) 
         
         if( avgbol == true ) {
             queryResults.forEach(r=>{ if(r.data && r.data[0] && (r.data[0].metadata.name == "avg") ){linechartdata.push(r.data[0])}})
